9812917a38e4e9339d7622509e7058a331a122c5
Anders Karlsen <karlsenanders@hotmail.com>
2013-06-02 13:48:10 +0200

    Create gamer page

--- src/main/java/no/anderska/wta/PlayerServlet.java
+++ src/main/java/no/anderska/wta/PlayerServlet.java
@@ -1,6 +1,7 @@
 package no.anderska.wta;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 public class PlayerServlet extends HttpServlet {
 
     private PlayerHandler playerHandler;
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer
+            .append("<form method='POST' action='createGamer.html'>") //
+            .append("<input type='text' name='gamerName' value=''/>") //
+            .append("<input type='submit' name='createGamer' value=''/>") //
+            .append("</form>") //
+        ;
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
--- src/test/java/no/anderska/wta/PlayerServletTest.java
+++ src/test/java/no/anderska/wta/PlayerServletTest.java
@@ -1,21 +1,44 @@
 package no.anderska.wta;
 
+import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.dom4j.DocumentHelper;
 import org.junit.Test;
 
 public class PlayerServletTest {
     private HttpServletRequest req = mock(HttpServletRequest.class);
     private HttpServletResponse resp = mock(HttpServletResponse.class);
+    private PlayerServlet servlet = new PlayerServlet();
 
     @Test
+    public void shouldDisplayCreatePlayerPage() throws Exception {
+        when(req.getMethod()).thenReturn("GET");
+        
+        StringWriter htmlDoc = new StringWriter();
+        when(resp.getWriter()).thenReturn(new PrintWriter(htmlDoc));
+        
+        servlet.service(req, resp);
+
+        assertThat(htmlDoc.toString()) //
+            .contains("<form method='POST' action='createGamer.html'") //
+            .contains("<input type='text' name='gamerName' value=''") //
+            .contains("<input type='submit' name='createGamer' value=''") //
+            ;
+
+        DocumentHelper.parseText(htmlDoc.toString());
+    }
+    
+    @Test
     public void shouldAddPlayer() throws Exception {
-        PlayerServlet servlet = new PlayerServlet();
         when(req.getParameter("gamerName")).thenReturn("Gamers");
         when(req.getMethod()).thenReturn("POST");
         
@@ -28,4 +51,6 @@ public class PlayerServletTest {
         verify(playerHandler).createPlayer("Gamers");
         verify(resp).sendRedirect("/");
     }
+    
+    
 }
