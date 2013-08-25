95e33905ffff10bc6968487c4c502182d849553f
Anders Karlsen <karlsenanders@hotmail.com>
2013-08-25 14:57:23 +0200

    Category page displaying dummy data

---
+++ src/main/webapp/js/categoriesCtrl.js
@@ -0,0 +1,4 @@
+function CategoriesCtrl($scope) {
+    $scope.categories = [{"id":2,"description":"The answer is the sum. Question: \u00274+2+7\u0027, Correct answer \u002713\u0027"},{"id":1,"description":"The answer is the same as the question. Question: \u0027hello\u0027, Correct answer \u0027hello\u0027"}];
+};
+
