98d4ada8ad1e0306bbe459cb2212d8998b171d1b
Anders Karlsen <karlsenanders@hotmail.com>
2013-08-25 16:07:55 +0200

    Displaying server data in table

--- src/main/webapp/js/categoriesCtrl.js
+++ src/main/webapp/js/categoriesCtrl.js
@@ -1,4 +1,11 @@
-function CategoriesCtrl($scope) {
-    $scope.categories = [{"id":2,"description":"The answer is the sum. Question: \u00274+2+7\u0027, Correct answer \u002713\u0027"},{"id":1,"description":"The answer is the same as the question. Question: \u0027hello\u0027, Correct answer \u0027hello\u0027"}];
+function CategoriesCtrl($scope,$http) {
+    $scope.categories = [];
+    //$scope.categories = [{"id":2,"description":"The answer is the sum. Question: \u00274+2+7\u0027, Correct answer \u002713\u0027"},{"id":1,"description":"The answer is the same as the question. Question: \u0027hello\u0027, Correct answer \u0027hello\u0027"}];
+
+    $http({method: "GET", url: "game"})
+        .success(function(data) {
+           $scope.categories = data;
+        });
+
 };
 
