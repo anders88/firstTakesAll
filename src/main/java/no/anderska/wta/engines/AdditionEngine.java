58de1c56ea4f3aa8bab59a5702997ed9f09975c9
Anders Karlsen <karlsenanders@hotmail.com>
2013-09-01 11:57:04 +0200

    Implemented engines

--- src/main/java/no/anderska/wta/engines/AdditionEngine.java
+++ src/main/java/no/anderska/wta/engines/AdditionEngine.java
@@ -1,45 +1,62 @@
 package no.anderska.wta.engines;
 
+import no.anderska.wta.game.Engine;
+import no.anderska.wta.game.Question;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-public class AdditionEngine {
+public class AdditionEngine implements Engine {
 
-	private static class SumQuestion {
-		private List<Integer> factors;
+    @Override
+    public List<Question> generateQuestions(String playerid) {
+        Random random = new Random();
+        List<Question> questions = new ArrayList<>();
+        for (int i=1;i<=numberOfQuestions;i++) {
+            StringBuilder question = new StringBuilder();
+            int numFactors = (maxFactors == 2) ? 2 : random.nextInt(maxFactors-1)+2;
+            int answer = 0;
+            for (int j=0;j<numFactors;j++) {
+                if (j > 0) {
+                    question.append("+");
+                }
+                int factor = random.nextInt(999) + 1;
+                answer+=factor;
+                question.append(factor);
+            }
+            questions.add(new Question(question.toString(),"" + answer));
+        }
+        return questions;
+    }
 
-		public SumQuestion(List<Integer> factors) {
-			this.factors = factors;
-		}
-		
-		public List<Integer> getFactors() {
-			return factors;
-		}
-		
-	}
-	
-	private Map<Integer, SumQuestion> questions = new HashMap<Integer, AdditionEngine.SumQuestion>();
-	
-	public AdditionEngine(int numberOfQuestions, int maxFactors) {
-		Random random = new Random();
-		if (numberOfQuestions <= 0) {
-			throw new IllegalArgumentException("Number of questions must be positive");
-		}
-		if (maxFactors < 2) {
-			throw new IllegalArgumentException("Number of factors must be 2 or greater");
-		}
-		for (int i=1;i<=numberOfQuestions;i++) {
-			List<Integer> factors = new ArrayList<>();
-			
-			int numFactors = (maxFactors == 2) ? 2 : random.nextInt(maxFactors-1)+2;
-			for (int j=0;j<numFactors;j++) {
-				factors.add(random.nextInt(999)+1);
-			}
-			questions.put(i, new SumQuestion(factors));
-		}
+    @Override
+    public String description() {
+        return "The answer is the sum. Question: '4+2+7', Correct answer '13'";
+    }
+
+    @Override
+    public int points() {
+        return 3;
+    }
+
+
+
+    private int numberOfQuestions;
+    private final int maxFactors;
+
+    public AdditionEngine(int numberOfQuestions, int maxFactors) {
+        if (maxFactors < 2) {
+            if (numberOfQuestions <= 0) {
+                throw new IllegalArgumentException("Number of questions must be positive");
+            }
+            throw new IllegalArgumentException("Number of factors must be 2 or greater");
+        }
+        this.numberOfQuestions = numberOfQuestions;
+        this.maxFactors = maxFactors;
+
 		
 	}
 
--- src/main/java/no/anderska/wta/engines/EchoEngine.java
+++ src/main/java/no/anderska/wta/engines/EchoEngine.java
@@ -1,26 +1,40 @@
 package no.anderska.wta.engines;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import no.anderska.wta.game.Engine;
+import no.anderska.wta.game.Question;
 
-public class EchoEngine {
-	
-	private Map<Integer, String> questions = new HashMap<>();
-	
-	public EchoEngine(int numberOfQuestions) {
-		if (numberOfQuestions <= 0) {
+import java.util.*;
+
+public class EchoEngine implements Engine {
+
+    private int numberOfQuestions;
+
+    public EchoEngine(int numberOfQuestions) {
+        this.numberOfQuestions = numberOfQuestions;
+        if (numberOfQuestions <= 0) {
 			throw new IllegalArgumentException("Number of questions must be positive");
 		}
-		for (int i=1;i<=numberOfQuestions;i++) {
-			questions.put(i, UUID.randomUUID().toString());
-		}
 	}
 
 
-	public String getDescription() {
-		return "The answer is the same as the question. Question: 'hello', Correct answer 'hello'";
-	}
+    @Override
+    public List<Question> generateQuestions(String playerid) {
 
+        List<Question> questions = new ArrayList<>(numberOfQuestions);
+        for (int i=0;i<numberOfQuestions;i++) {
+            String rand = UUID.randomUUID().toString();
+            questions.add(new Question(rand,rand));
+        }
+        return questions;
+    }
 
+    @Override
+    public String description() {
+        return "The answer is the same as the question. Question: 'hello', Correct answer 'hello'";
+    }
+
+    @Override
+    public int points() {
+        return 1;
+    }
 }
