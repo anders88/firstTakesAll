9caf8b52d8764b0c6a933d742b2dee9d8bc09a55
Anders Karlsen <karlsenanders@hotmail.com>
2013-09-07 22:22:30 +0200

    Sorting tables

--- src/main/webapp/js/categoriesCtrl.js
+++ src/main/webapp/js/categoriesCtrl.js
@@ -1,9 +1,24 @@
 function CategoriesCtrl($scope,$http) {
-    $scope.gameStatus = [];
+    var sortPlayers = function(players) {
+        return _.sortBy(players,function(player) {
+            return 0-player.points;
+        });
+    };
+
+    var sortCategories = function(categories) {
+        return _.sortBy(categories,function(category) {
+            return category.points;
+        });
+    };
+
+    $scope.players = [];
+    $scope.categories = [];
+
 
     $http({method: "GET", url: "status"})
         .success(function(data) {
-           $scope.gameStatus = data;
+           $scope.players = sortPlayers(data.players);
+           $scope.categories = sortCategories(data.categories);
         });
 
     setInterval(function() {
@@ -11,8 +26,8 @@ function CategoriesCtrl($scope,$http) {
         var urlstr = "status?dummy=" + time;
         $http({method: "GET", url: urlstr})
             .success(function(data) {
-                $scope.gameStatus = data;
-                $scope.$apply();
+                $scope.players = sortPlayers(data.players);
+                $scope.categories = sortCategories(data.categories);
             });
     },15000);
 
