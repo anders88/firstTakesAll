baa9a0d6bf12a585d61638c6567f10b6a9201c81
Anders Karlsen <karlsenanders@hotmail.com>
2013-09-07 21:57:33 +0200

    Auto Reload

--- src/main/webapp/js/categoriesCtrl.js
+++ src/main/webapp/js/categoriesCtrl.js
@@ -6,5 +6,16 @@ function CategoriesCtrl($scope,$http) {
            $scope.gameStatus = data;
         });
 
+    setInterval(function() {
+        var time = new Date().getTime();
+        var urlstr = "status?dummy=" + time;
+        $http({method: "GET", url: urlstr})
+            .success(function(data) {
+                $scope.gameStatus = data;
+                $scope.$apply();
+            });
+    },15000);
+
+
 };
 
