ad4f0fe980d6ad893e5a0cf2d4b741a3a45e3f60
Anders Karlsen <karlsenanders@gmail.com>
2013-09-14 11:47:17 +0200

    Sudoko check

--- src/test/java/no/anderska/wta/engines/SudokoEngineTest.java
+++ src/test/java/no/anderska/wta/engines/SudokoEngineTest.java
@@ -2,6 +2,8 @@ package no.anderska.wta.engines;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.fest.assertions.Assertions.assertThat;
 
 public class SudokoEngineTest {
@@ -28,8 +30,61 @@ public class SudokoEngineTest {
 
 
         assertThat(board[0][0]).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldHandleIllegalBoards() throws Exception {
+        assertThat(sudokoEngine.readBoard(null,"\n")).isNull();
+        assertThat(sudokoEngine.readBoard("","\n")).isNull();
+
+        String boardStr =
+                "123a56789\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "---------\n" +
+                        "345678912\n";
+
+        assertThat(sudokoEngine.readBoard(boardStr,"\n")).isNull();
+
+        boardStr =
+                "123456789\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "---------\n" +
+                        "345678912\n";
+
+        assertThat(sudokoEngine.readBoard(boardStr,"\n")).isNull();
+
+        boardStr =
+                "12356789\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "234567891\n" +
+                        "---------\n" +
+                        "345678912\n";
+
+        assertThat(sudokoEngine.readBoard(boardStr,"\n")).isNull();
 
     }
 
+    @Test
+    public void shouldGetBoard() throws Exception {
+        List<Integer[][]> boards = sudokoEngine.readBoardsFromResource("sudokoTestBoards.txt");
+        assertThat(boards).hasSize(3);
 
+        assertThat(sudokoEngine.isLegal(boards.get(0))).isTrue();
+        assertThat(sudokoEngine.isLegal(boards.get(1))).isFalse();
+        assertThat(sudokoEngine.isLegal(boards.get(2))).isTrue();
+
+    }
 }
