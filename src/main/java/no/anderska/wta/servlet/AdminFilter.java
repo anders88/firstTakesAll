7ce39346616eda529cd442060a97c2d444c1410f
Anders Karlsen <karlsenanders@hotmail.com>
2013-10-05 17:20:07 +0200

    AdminFilter invoked

--- src/main/java/no/anderska/wta/servlet/AdminFilter.java
+++ src/main/java/no/anderska/wta/servlet/AdminFilter.java
@@ -1,20 +1,34 @@
 package no.anderska.wta.servlet;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AdminFilter implements Filter {
+    private SecurityHandler securityHandler;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        this.securityHandler = new SecurityHandler();
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("Here is filter");
-        chain.doFilter(request,response);
+        HttpServletRequest req = (HttpServletRequest) request;
+        if (securityHandler.isLoggedIn(req.getSession())) {
+            chain.doFilter(request, response);
+        } else {
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.sendRedirect("login");
+        }
     }
 
     @Override
     public void destroy() {
+    }
+
+    public void setSecurityHandler(SecurityHandler securityHandler) {
+        this.securityHandler = securityHandler;
     }
 }
---
+++ src/test/java/no/anderska/wta/servlet/AdminFilterTest.java
@@ -0,0 +1,47 @@
+package no.anderska.wta.servlet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import static org.mockito.Mockito.*;
+
+public class AdminFilterTest {
+    private final HttpServletRequest req = mock(HttpServletRequest.class);
+    private final HttpServletResponse resp = mock(HttpServletResponse.class);
+    private final AdminFilter adminFilter = new AdminFilter();
+    private final FilterChain filterChain = mock(FilterChain.class);
+    private final SecurityHandler securityHandler = mock(SecurityHandler.class);
+    private final HttpSession session = mock(HttpSession.class);
+
+
+    @Before
+    public void setUp() throws Exception {
+        when(req.getSession()).thenReturn(session);
+        adminFilter.setSecurityHandler(securityHandler);
+    }
+
+    @Test
+    public void shouldAdminWhenLoggedIn() throws Exception {
+        when(securityHandler.isLoggedIn(session)).thenReturn(true);
+
+        adminFilter.doFilter(req, resp, filterChain);
+
+
+        verify(filterChain).doFilter(req, resp);
+    }
+
+    @Test
+    public void shouldDenyAccessWhenNotLoggedIn() throws Exception {
+        when(securityHandler.isLoggedIn(session)).thenReturn(false);
+
+        adminFilter.doFilter(req, resp, filterChain);
+
+        verify(filterChain, never()).doFilter(req, resp);
+        verify(resp).sendRedirect("login");
+    }
+}
