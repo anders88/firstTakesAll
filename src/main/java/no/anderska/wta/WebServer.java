b0b88d29b21a139f6bde4f919acf5ecdb350d9f5
Johannes Brodwall <jbr@exilesoft.com>
2013-10-15 15:16:00 +0200

    Upgraded Jetty to simplify restart

--- src/main/java/no/anderska/wta/WebServer.java
+++ src/main/java/no/anderska/wta/WebServer.java
@@ -2,15 +2,34 @@ package no.anderska.wta;
 
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ShutdownHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class WebServer {
 
-	public static void main(String[] args) throws Exception {
-		String portString = System.getenv("PORT");
-		Server server = new Server(Integer.valueOf(portString == null ? "8081" : portString));
-		server.setHandler(new WebAppContext("src/main/webapp", "/"));
+	private final Integer port;
+
+    public WebServer(Integer port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+		new WebServer(getPort(8081)).start();
+	}
+
+    private void start() throws Exception {
+        Server server = new Server(port);
+        HandlerList handlerList = new HandlerList();
+		handlerList.addHandler(new ShutdownHandler("sndglskdngsolnlan", false, true));
+		handlerList.addHandler(new WebAppContext("src/main/webapp", "/"));
+        server.setHandler(handlerList);
 		server.start();
 		System.out.println(server.getURI());
-	}
+    }
+
+    private static Integer getPort(int defaultPort) {
+        String envPort = System.getenv("PORT");
+        return Integer.valueOf(envPort == null ? String.valueOf(defaultPort) : envPort);
+    }
 }
