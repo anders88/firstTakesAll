b6ad7a9d018af6784bde904658c982b6df2506a4
Anders Karlsen <karlsenanders@gmail.com>
2013-10-23 19:46:19 +0200

    Handling but too slow for large grids

--- src/main/java/no/anderska/wta/questions/ts/TravellingSalesmanSolver.java
+++ src/main/java/no/anderska/wta/questions/ts/TravellingSalesmanSolver.java
@@ -7,28 +7,47 @@ import java.util.Set;
 public class TravellingSalesmanSolver {
     private Set<Integer> allCities;
     private List<Integer> visited;
+    private List<Integer> bestRoute;
+    private int best;
+    private int sum;
+    private CityDistances cityDistances;
+
 
     public synchronized List<Integer> compute(CityDistances cityDistances, int start) {
+        this.cityDistances = cityDistances;
         allCities = cityDistances.cities();
         visited = new ArrayList<>();
 
-        allCities.add(start);
+        visited.add(start);
+        best = Integer.MAX_VALUE;
+        sum = 0;
+
         walk();
 
-        return visited;
+        return bestRoute;
     }
 
     private void walk() {
         if (visited.size() == allCities.size()) {
-            visited.add(visited.get(0));
+            bestRoute = new ArrayList<>(visited);
+            bestRoute.add(visited.get(0));
+            best = sum;
             return;
         }
-        for (int next : allCities) {
-            if (visited.contains(next)) {
-                continue;
+        if (sum < best) {
+            for (int next : allCities) {
+                if (visited.contains(next)) {
+                    continue;
+                }
+                int distance = cityDistances.get(visited.get(visited.size()-1),next);
+                sum = sum + distance;
+                visited.add(next);
+
+                walk();
+
+                visited.remove(visited.size()-1);
+                sum = sum - distance;
             }
-            visited.add(next);
-            walk();
         }
 
     }
--- src/test/java/no/anderska/wta/questions/ts/TravellingSalesmanSolverTest.java
+++ src/test/java/no/anderska/wta/questions/ts/TravellingSalesmanSolverTest.java
@@ -2,6 +2,8 @@ package no.anderska.wta.questions.ts;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.fest.assertions.Assertions.assertThat;
 
 public class TravellingSalesmanSolverTest {
@@ -12,8 +14,33 @@ public class TravellingSalesmanSolverTest {
     @Test
     public void twoCitiesHasOneSolution() throws Exception {
         cityDistances.add(1,2,5);
-        assertThat(solver.compute(cityDistances, 1)).containsExactly(1,2,1);
+        assertThat(solver.compute(cityDistances, 1)).containsExactly(1, 2, 1);
     }
 
+    @Test
+    public void findPathWithFourCities() throws Exception {
+        cityDistances.add(1,2,50);
+        cityDistances.add(1,3,1);
+        cityDistances.add(1,4,1);
+        cityDistances.add(2,3,1);
+        cityDistances.add(2,4,1);
+        cityDistances.add(3,4,50);
+        assertThat(solver.compute(cityDistances, 1)).containsExactly(1,4,2,3,1);
+    }
 
+    private void randomize(int numNodes) {
+        Random rnd = new Random();
+        for (int from=1;from<numNodes;from++) {
+            for (int to=from+1;to<=numNodes;to++) {
+                cityDistances.add(from,to,rnd.nextInt(40)+1);
+            }
+        }
+    }
+
+    @Test
+    public void shuoldHandleBiggerBoards() throws Exception {
+        randomize(12);
+        solver.compute(cityDistances,1);
+
+    }
 }
